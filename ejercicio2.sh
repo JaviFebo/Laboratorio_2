352abefadf3e1a518bc2b447ae5447a19075084e
JaviFebo <javi.bolanos.c@gmail.com>
2025-09-10 13:53:40 -0600

    Lectura y registro de consumo de memoria y cpu añadidos

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-proceso=$1
+proceso=$@
 
 proceso_existe() {
 	command -v $proceso >/dev/null
 }
 
-if [ $# == 0 ]; then
+cpu_proceso() {
+	ps -o %cpu -p $id_proceso | tail -n +2
+}
+
+mem_proceso() {
+	ps -o %mem -p $id_proceso | tail -n +2
+}
+
+proceso_corriendo() {
+	ps -p $id_proceso > /dev/null	
+}
+
+if [ $# -eq 0 ]; then
 	echo "Error: ingrese un proceso"
 	exit 1
 fi
 
-if proceso_existe; then
-	$proceso
-
-else
+if ! proceso_existe; then
 	echo "Error: proceso inválido"
 	exit 1
 fi
+
+$proceso &
+id_proceso=$!
+
+> log_cpu.txt
+> log_mem.txt
+
+while proceso_corriendo; do
+	cpu_proceso >> log_cpu.txt
+	mem_proceso >> log_mem.txt
+	sleep 1
+done
