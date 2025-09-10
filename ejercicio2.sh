70d76ef2d59ab4e827b642b0ca3b24a0ffa0c905
JaviFebo <javi.bolanos.c@gmail.com>
2025-09-10 00:11:30 -0600

    Lectura y verificación de proceso

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+proceso=$1
+
+proceso_existe() {
+	command -v $proceso >/dev/null
+}
+
+if [ $# == 0 ]; then
+	echo "Error: ingrese un proceso"
+	exit 1
+fi
+
+if proceso_existe; then
+	$proceso
+
+else
+	echo "Error: proceso inválido"
+	exit 1
+fi
