f4631f267866e88c3e0a53dad689d1eb8fbcddf3
JaviFebo <javi.bolanos.c@gmail.com>
2025-09-10 20:42:29 -0600

    Función de registro de fecha y hora del evento añadida

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 directorio=$1
+archivo_log="log_notify.txt"
 
 monitorear() {
-	inotifywait -m -q -e create,delete,modify $directorio
+	inotifywait -m -q -e create,delete,modify "$directorio"
 }
 
-monitorear
+registrar_tiempo() {
+	while read -r evento; do
+    		fecha=$(date '+%Y-%m-%d %H:%M:%S')
+    		echo "$evento" | awk -v d="$fecha" '{ print d, $0 }'
+	done
+}
+
+monitorear | registrar_tiempo >> "$archivo_log"
